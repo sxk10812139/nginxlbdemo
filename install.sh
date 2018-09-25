24dbb1e3f00cc32b430640b83c7dc18e1f5bbdf7
Mu <sxk10812139@163.com>
2018-09-25 20:36:59 +0800

    add flb

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@ docker network create mynet
 docker run -tid -p 8001:80 --net mynet --name s1 nginx
 docker run -tid -p 8002:80 --net mynet --name s2 nginx
 docker run -tid -p 8003:80 --net mynet --name s3 nginx
-docker run -tid -p 8004:80 -v=$(pwd)/nginx.conf:/etc/nginx/nginx.conf -v=$(pwd)/upstream.conf:/etc/nginx/upstream.conf --net mynet --name lb nginx&&docker ps -a 
+docker run -tid -p 8004:80 -v=$(pwd)/lb.conf:/etc/nginx/nginx.conf -v=$(pwd)/upstream.conf:/etc/nginx/upstream.conf --net mynet --name lb nginx 
+docker run -tid -p 8005:80 -v=$(pwd)/flb.conf:/etc/nginx/nginx.conf --net mynet --name flb nginx&&docker ps -a 
 
 docker exec s1 /bin/bash -c "echo s1 > /usr/share/nginx/html/index.html"
 docker exec s2 /bin/bash -c "echo s2 > /usr/share/nginx/html/index.html"
 docker exec s3 /bin/bash -c "echo s3 > /usr/share/nginx/html/index.html"
+docker exec s1 /bin/bash -c "apt-get update&&apt-get install -y tcpdump net-tools"
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker rm -f s1 s2 s3 lb
+docker rm -f s1 s2 s3 lb flb
 docker network rm mynet
